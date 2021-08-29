777037b2058b2bb5ed282b31faaef8f654beae65
Gonçalo <goncalo-lopes@hotmail.com>
2021-08-29 18:52:00 +0100

    Agosto

--- CrowdZero_Android/app/src/main/java/com/example/crowdzero_android/MainActivity.java
+++ CrowdZero_Android/app/src/main/java/com/example/crowdzero_android/MainActivity.java
@@ -1,13 +1,48 @@
 package com.example.crowdzero_android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView c1, c2, c3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        c1 = findViewById(R.id.btnPoints);
+        c2 = findViewById(R.id.btnPerfil);
+
+        c1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.btnPoints:
+                        break;
+                }
+            }
+        });
+
+        c2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.btnPerfil:
+                        openPerfil();
+                        break;
+                }
+            }
+        });
+    }
+
+    protected void openPerfil(){
+        startActivity(new Intent(MainActivity.this, PerfilActivity.class));
+        finish();
     }
 }
